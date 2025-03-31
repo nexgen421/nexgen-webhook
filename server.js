477e0ac3158031af66a41928f20398f68b6c8f2e
nexgen421 <nexgencourierservice@gmail.com>
2025-04-01 00:55:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ app.use(express.json());
 
 // Routes
 app.use('/webhook', webhookRoutes);
-
+app.get('/',(req,res)=>{
+  res.status(200).json({messgae:"Hello World"})
+})
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, async () => {
