0787d7226a7d751240e81022fac0d367d31e694a
nexgen421 <nexgencourierservice@gmail.com>
2025-04-01 01:30:49 +0530

    Update index.js

--- models/index.js
+++ models/index.js
@@ -4,6 +4,12 @@ const db_url = process.env.DB_URL
 const sequelize = new Sequelize(db_url, {
     dialect: "postgres",
     dialectModule: require("pg"),
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false, // For self-signed certificates
+        },
+    },
     logging: false, // Optional: Disable logging
 });
 
