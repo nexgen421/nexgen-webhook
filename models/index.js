4c4c077b57773a4cb561d3a7f9eed9ed0e2fff76
nexgen421 <nexgencourierservice@gmail.com>
2025-04-01 01:14:30 +0530

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,7 +1,11 @@
 const { Sequelize, DataTypes } = require('sequelize');
 require('dotenv').config()
 const db_url = process.env.DB_URL
-const sequelize = new Sequelize(db_url);
+const sequelize = new Sequelize(db_url, {
+    dialect: "postgres",
+    dialectModule: require("pg"),
+    logging: false, // Optional: Disable logging
+});
 
 const Tracking = require('./tracking')(sequelize, DataTypes);
 
